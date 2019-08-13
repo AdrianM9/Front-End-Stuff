19fca2524bc62d5ac6e4c8c6b2099132bae0e5a6
Adrian Mirea <amirea99@gmail.com>
2019-08-13 15:54:14 +0300

    Added Router and initial pages

---
+++ src/components/Router.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as ReactRouter, Route, Switch } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
+
+import Homepage from "./Homepage";
+import DualButton from "./DualButton";
+
+function RouterWrapper() {
+  return <Router />;
+}
+
+function Router() {
+  return (
+    <ReactRouter>
+      <Switch>
+        <Route exact path={ROUTES.HOMEPAGE} component={Homepage} />
+        <Route path={ROUTES.DUAL_BUTTON} component={DualButton} />
+      </Switch>
+    </ReactRouter>
+  );
+}
+
+export default RouterWrapper;
