2bd28bfb1d0f0f8e49ecae15a0edf74a6eb478b9
Adrian Mirea <amirea99@gmail.com>
2019-08-13 16:15:33 +0300

    Styled-components + design changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
 import Router from "./components/Router";
 import * as serviceWorker from "./serviceWorker";
 
-const App = () => <Router />;
+const NetlifyStatus = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0.8;
+  height: 10px;
+`;
+
+const App = () => {
+  return (
+    <>
+      <Router />
+      <NetlifyStatus
+        src="https://api.netlify.com/api/v1/badges/6ab1c1e7-b90b-44d9-9ef4-33b39d1a2972/deploy-status"
+        alt="status"
+      />
+    </>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
